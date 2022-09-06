5455fd43248587bb3337718f0c3ea87dc11f27ec
Dawson W <aciisquid@gmail.com>
2022-09-06 16:26:46 -0600

    basic page and dependencies

---
+++ app/src/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+
+function Main() {
+    return (
+        <div className="Main">
+            <header className="Head">
+                Chromemaster
+            </header>
+
+        </div>
+    )
+}
