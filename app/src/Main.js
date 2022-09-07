f7f0fffa9591564d37d2af0bc79596b59181a43d
Dawson W <aciisquid@gmail.com>
2022-09-06 19:27:10 -0600

    setup router pages

--- app/src/Main.js
+++ app/src/Main.js
@@ -1,4 +1,10 @@
-import './Main.css';
+import './App.css';
+
+import { Link, Route, Routes } from "react-router-dom";
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
 
 function Main() {
     return (
@@ -6,7 +12,21 @@ function Main() {
             <header className="Head">
                 Chromemaster
             </header>
-
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/products">Products</Link>
+            </nav>
+            <hr />
+            <div>
+                <Routes>
+                    <Route path="/" element={ <Home /> }></Route>
+                    <Route path="/about" element={ <About /> }></Route>
+                    <Route path="/products" element={ <Products /> }></Route>
+                </Routes>
+            </div>
         </div>
     )
 }
+
+export default Main;
